0197ccb5d0801ffab1a5a0c9c705fd33b0e42ef1
Joachim Neubert <j.neubert@zbw.eu>
2018-06-26 15:31:49 +0200

    Add value types report

--- bin/get_statistics.pl
+++ bin/get_statistics.pl
@@ -23,6 +23,8 @@ my %endpoint = (
   Openlink => 'http://lod.openlinksw.com/sparql/',
   LOV      => 'http://lov.okfn.org/dataset/lov/sparql',
 );
+my $value_type_store = 'Openlink';
+my @value_types      = qw/ IRI Literal Blank /;
 
 # configuration
 my %config = (
@@ -30,33 +32,43 @@ my %config = (
     names => [],
     query => "select (str(count(*)) as ?count) ?class
 where {
-  values ( ?class ) {
-    ( VALUE )
-  }
+  bind(VAR as ?class)
   ?s a ?class .
 }
 group by ?class
   ",
   },
-
   property => {
     names => [],
     query => "select (str(count(*)) as ?count) ?property
 where {
-  values ( ?property ) {
-    ( VALUE )
-  }
+  bind(VAR as ?property)
   ?s ?property [] .
 }
 group by ?property
   ",
-  }
+  },
+
+  # special type, only query used
+  property_value => {
+    query => "select (str(count(*)) as ?count) ?property
+where {
+  bind(VAR as ?property)
+  ?s ?property ?value .
+  # FILTER #
+}
+group by ?property
+  ",
+  },
 );
 
 # initialize data structures
 my $client = REST::Client->new();
 my %term;
 get_names( \%config, \%term );
+
+# debug
+##ä$config{property}{names} = [qw / contributor type /];
 
 # get and save all counts
 foreach my $type (qw/ class property/) {
@@ -69,6 +81,18 @@ foreach my $type (qw/ class property/) {
   }
   save_as_csv( $type, \@names );
 }
+
+# get property value types
+my @names = @{ $config{property}{names} };
+foreach my $name (@names) {
+  print "  get value types for $name ...\n";
+  foreach my $value_type (qw/ IRI Literal Blank /) {
+    get_count( 'property_value', $name, $value_type_store, \%term,
+      $value_type );
+  }
+}
+##print Dumper \%term;
+save_as_csv( 'property_value', \@names );
 
 ##########################################
 
@@ -103,10 +127,14 @@ sub get_count {
   my $name     = shift or die "param missing\n";
   my $store    = shift or die "param missing\n";
   my $term_ref = shift or die "param missing\n";
+  my $value_type = shift;
 
   # prepare query
   my $query = 'query=' . $config{$type}{query};
-  $query =~ s/VALUE/<$term{$name}{uri}>/;
+  $query =~ s/VAR/<$term{$name}{uri}>/;
+  if ( $type eq 'property_value' ) {
+    $query =~ s/# FILTER #/filter(is$value_type(?value))/;
+  }
 
   # execute query
   $client->GET( $endpoint{$store} . "?$query",
@@ -117,6 +145,7 @@ sub get_count {
       $term_ref->{$name}{count}{$store} = '"timeout"';
       return;
     }
+
     # known problem with LOD4ALL and IMT
     if ( $store eq 'LOD4ALL' && $client->responseCode() eq '500' ) {
       $term_ref->{$name}{count}{$store} = '"syserr"';
@@ -129,7 +158,17 @@ sub get_count {
 
   # save count
   my $count = $result_ref->{results}{bindings}[0]{count}{value} || "0";
-  $term_ref->{$name}{count}{$store} = $count;
+
+  if ( $type eq 'property_value' ) {
+    ##my $percentage = sprintf( "%.2f",
+    ##  ( $count / $term_ref->{$name}{count}{$value_type_store} ) * 100 );
+    ##$term_ref->{$name}{value_type}{$value_type} = $percentage;
+
+    # for now, just use the count
+    $term_ref->{$name}{value_type}{$value_type} = $count;
+  } else {
+    $term_ref->{$name}{count}{$store} = $count;
+  }
   return;
 }
 
@@ -139,13 +178,29 @@ sub save_as_csv {
 
   my $fn = "../statistics/terms_$type.csv";
   open( my $fh, '>', $fn ) or die "Could not open $fn: $!\n";
-  print $fh '"', join( '","', $type, @stores ), "\"\n";
+
+  if ( $type eq 'property_value' ) {
+    print $fh '"',
+      join( '","',
+      'value types (in %)',
+      "Total (in $value_type_store)", @value_types ),
+      "\"\n";
+  } else {
+    print $fh '"', join( '","', $type, @stores ), "\"\n";
+  }
   foreach my $name ( @{$names_ref} ) {
-    my @counts = ("\"$name\"");
-    foreach my $store (@stores) {
-      push( @counts, $term{$name}{count}{$store} );
+    my @fields = ("\"$name\"");
+    if ( $type eq 'property_value' ) {
+      push( @fields, $term{$name}{count}{$value_type_store} );
+      foreach my $value_type (@value_types) {
+        push( @fields, $term{$name}{value_type}{$value_type} );
+      }
+    } else {
+      foreach my $store (@stores) {
+        push( @fields, $term{$name}{count}{$store} );
+      }
     }
-    print $fh join( ',', @counts ), "\n";
+    print $fh join( ',', @fields ), "\n";
   }
   close $fh;
   print "Saved $fn\n";
